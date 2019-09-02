e2b846b4d91e143a1540b6ff1f48719e69710ff1
Travis <travisformayor@users.noreply.github.com>
2019-09-02 16:42:29 -0700

    added test api fetch and display results

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"decred-roi-api/datab"
+	"decred-roi-api/fetchapi"
 )
 
 // This method required adding a trust record to pg_hba.conf
@@ -18,11 +19,18 @@ const (
 func main() {
 	dbObj := datab.Database{}
 	dbObj.Connect()
-	result := dbObj.ReadDB(1)
+	//testConnect, err := datab.DBTest()
+	result1 := dbObj.ReadDB(1)
 	result2 := dbObj.ReadDB(10)
 
-	fmt.Println(result)
+	fmt.Println(result1)
 	fmt.Println(result2)
 
-	//testConnect, err := datab.DBTest()
+	// Create new instance of the People struct
+	fetchObj := fetchapi.People{}
+	// Call the TestGet method for the People struct instance
+	result3 := fetchObj.TestGet()
+
+	fmt.Printf("%d people are currently in space\n", result3.HowMany)
+	fmt.Printf("Example: %s is on the %s\n", result3.Persons[0].Name, result3.Persons[0].Craft)
 }
