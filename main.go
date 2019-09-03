47bebd7f99997b41d62179a05fcc5b985e5ce275
Travis <travisformayor@users.noreply.github.com>
2019-09-02 17:06:32 -0700

    remove old db info

--- main.go
+++ main.go
@@ -6,16 +6,6 @@ import (
   "decred-roi-api/fetchapi"
 )
 
-// This method required adding a trust record to pg_hba.conf
-// Is there a more secure way?
-const (
-  host     = "127.0.0.1"
-  port     = 5432
-  user     = "dcrtest"
-  password = "testPassword"
-  dbname   = "decred_test"
-)
-
 func main() {
   dbObj := datab.Database{}
   dbObj.Connect()
